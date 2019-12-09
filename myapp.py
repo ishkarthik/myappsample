a9019bc84cbc43373612205dba0a59090f655749
Karthik Ramachandran <karthikeyan.ramachandran@gmail.com>
2019-12-09 12:01:42 -0800

    Initial commit

---
+++ myapp.py
@@ -0,0 +1,10 @@
+def helloworld();
+    resp = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": "mittu mani mahi puppy"
+    }
+
+    return resp
